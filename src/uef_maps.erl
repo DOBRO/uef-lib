8adaf1931f8748d80f5014f11d453933187c25a9
Sergei Semichev <chessvegas@chessvegas.com>
2019-05-15 16:04:58 +0300

    Rewrite function uef_maps:find_nested/2

--- src/uef_maps.erl
+++ src/uef_maps.erl
@@ -10,21 +10,26 @@
 %% find_nested/3
 -spec find_nested(Keys :: [], map()) -> {ok, term()} | error.
 find_nested(Keys, Map) ->
-	find_nested(Keys, Map, error).
+	find_nested_unsafe(Keys, Map).
 
 
 %%%------------------------------------------------------------------------------
 %%%   Internal functions
 %%%------------------------------------------------------------------------------
 
-%% find_nested/3
--spec find_nested(Keys :: [], map(), Result) -> Result when Result :: {ok, term()} | error.
-find_nested([], _, Result) ->
+find_nested_unsafe(Keys, Map) ->
+	find_nested(Keys, Map, unsafe, error).
+
+%% find_nested/4
+-spec find_nested(Keys :: [], map(), safe | unsafe, Result) -> Result when Result :: {ok, term()} | error.
+find_nested([], _, _, Result) ->
 	Result;
-find_nested([Key|Tail], Map, _) ->
-	case maps:find(Key, Map) of
-		{ok, NestedMap} = Result ->
-			find_nested(Tail, NestedMap, Result);
-		_ ->
-			error
+find_nested([Key|Tail], Map, Safe, _) ->
+	Result = case {is_map(Map), Safe} of
+		{false, safe}  -> error; % don't fail, return 'error' safely
+		_ -> maps:find(Key, Map) % may fail here when {false, unsafe}
+	end,
+	case Result of
+		{ok, NestedMap} -> find_nested(Tail, NestedMap, Safe, Result);
+		error -> error
 	end.
