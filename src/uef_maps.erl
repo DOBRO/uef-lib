533e3245f90e12fda3c273f5e97319fa6401092e
Sergei Semichev <chessvegas@chessvegas.com>
2019-05-15 17:56:35 +0300

    Add tests for uef_maps

--- src/uef_maps.erl
+++ src/uef_maps.erl
@@ -3,6 +3,13 @@
 -export([find_nested/2]).
 -export([get_nested/2, get_nested/3]).
 
+%%%------------------------------------------------------------------------------
+%%%   EUnit
+%%%------------------------------------------------------------------------------
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
 
 %%%------------------------------------------------------------------------------
 %%%   Types
@@ -29,7 +36,7 @@ get_nested(Keys, Map) ->
 	FindResult = find_nested_unsafe(Keys, Map), % may fail here with a {badmap,Map} exception
 	case FindResult of
 		{ok, Value} -> Value;
-		error -> erlang:error({badkeys,Keys},[Keys,Map])
+		error -> erlang:error({notfound,Keys},[Keys,Map])
 	end.
 
 %% get_nested/3
@@ -42,6 +49,7 @@ get_nested(Keys, Map, Default) when is_map(Map) ->
 	end;
 get_nested(Keys, Map, Default) ->
 	erlang:error({badmap,Map},[Keys, Map, Default]).
+
 
 %%%------------------------------------------------------------------------------
 %%%   Internal functions
@@ -69,4 +77,75 @@ find_nested([Key|Tail], Map, Safe, _) ->
 	case Result of
 		{ok, NestedMap} -> find_nested(Tail, NestedMap, Safe, Result);
 		error -> error
-	end.
+	end;
+find_nested(NotList, _, _, _) ->
+	erlang:error({badlist, NotList}).
+
+
+%%%------------------------------------------------------------------------------
+%%%   Tests
+%%%------------------------------------------------------------------------------
+
+-ifdef(TEST).
+
+find_nested__and__get_nested_test_() ->
+	Value = value,
+	M3 = #{3 => Value},
+	M2 = #{2 => M3},
+	M1 = #{1 => M2},
+	M0 = #{0 => M1},
+	BadMap = bad_map,
+	BadList = bad_list,
+	Default = default,
+	[
+	% find_nested/2 test with {ok, _}
+	?_assertEqual({ok, M1}, find_nested([0], M0)),
+	?_assertEqual({ok, M2}, find_nested([0,1], M0)),
+	?_assertEqual({ok, M3}, find_nested([0,1,2], M0)),
+	?_assertEqual({ok, Value}, find_nested([0,1,2,3], M0)),
+	% find_nested/2 test with 'error'
+	?_assertEqual(error, find_nested([a], #{})),
+	?_assertEqual(error, find_nested([-1], M0)),
+	?_assertEqual(error, find_nested([-1, 1], M0)),
+	?_assertEqual(error, find_nested([1, -1], M0)),
+	?_assertEqual(error, find_nested([0,1,2,-3], M0)),
+	% find_nested/2 test with exception
+	?_assertError({badmap, BadMap}, find_nested([0], BadMap)),
+	?_assertError({badmap, BadMap}, find_nested([0, 1], #{0 => BadMap})),
+	?_assertError({badmap, BadMap}, find_nested([0, 1, 2], #{0 => #{1 => BadMap}})),
+	?_assertError({badmap, Value}, find_nested([0,1,2,3,4], M0)),
+	?_assertError({badmap, Value}, find_nested([0,1,2,3,4,5], M0)),
+	?_assertError({badlist, BadList}, find_nested(BadList, #{a => 1})),
+
+	% get_nested/2 test with Value
+	?_assertEqual(M1, get_nested([0], M0)),
+	?_assertEqual(M2, get_nested([0,1], M0)),
+	?_assertEqual(M3, get_nested([0,1,2], M0)),
+	?_assertEqual(Value, get_nested([0,1,2,3], M0)),
+	% get_nested/2 test with exception
+	?_assertError({notfound, [a]}, get_nested([a], M0)),
+	?_assertError({notfound, [0,-1]}, get_nested([0,-1], M0)),
+	?_assertError({notfound, [0,-1,2]}, get_nested([0,-1,2], M0)),
+	?_assertError({notfound, [0,1,2,-3]}, get_nested([0,1,2,-3], M0)),
+	?_assertError({badmap, Value}, get_nested([0,1,2,3,4], M0)),
+	?_assertError({badmap, Value}, get_nested([0,1,2,3,4,5], M0)),
+	?_assertError({badlist, BadList}, get_nested(BadList, #{a => 1})),
+
+	% get_nested/3 test with Value and Default
+	?_assertEqual(M1, get_nested([0], M0, Default)),
+	?_assertEqual(M2, get_nested([0,1], M0, Default)),
+	?_assertEqual(M3, get_nested([0,1,2], M0, Default)),
+	?_assertEqual(Value, get_nested([0,1,2,3], M0, Default)),
+	?_assertEqual(Default, get_nested([-1], M0, Default)),
+	?_assertEqual(Default, get_nested([0,-1], M0, Default)),
+	?_assertEqual(Default, get_nested([0,1,-2], M0, Default)),
+	?_assertEqual(Default, get_nested([0,1,2,-3], M0, Default)),
+	?_assertEqual(Default, get_nested([0,1,2,3,4], M0, Default)),
+	?_assertEqual(Default, get_nested([0,1,2,3,4,5], M0, Default)),
+	% get_nested/3 test with exception
+	?_assertError({badmap, BadMap}, get_nested([a], BadMap, Default)),
+	?_assertError({badlist, BadList}, get_nested(BadList, #{}, Default))
+	].
+
+
+-endif. % end of tests
