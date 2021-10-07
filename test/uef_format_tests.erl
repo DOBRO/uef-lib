4a69e6d58b93d453aa9a622c55ad8f7c89e89aac
Sergei Semichev <chessvegas@chessvegas.com>
2021-10-07 10:55:48 +0300

    Add more tests for uef_format module

--- test/uef_format_tests.erl
+++ test/uef_format_tests.erl
@@ -25,6 +25,9 @@ format_number_test_() ->
 	?_assertEqual(<<"1.00">>, uef_format:format_number(1, 2, 2, #{})),
 	?_assertEqual(<<"1.99">>, uef_format:format_number(1.99, 2, 2, #{})),
 	?_assertEqual(<<"2.00">>, uef_format:format_number(1.99, 1, 2, #{})),
+	?_assertEqual(<<"10">>, uef_format:format_number(10, 2, 0, #{})),
+	?_assertEqual(<<"1 000.00">>, uef_format:format_number(1000, 0, 2, #{thousands_sep => " "})),
+	?_assertEqual(<<"1 000.00">>, uef_format:format_number(1000, 0, 2, #{thousands_sep => [32]})),
 	?_assertEqual(<<"1 000 999.00">>, uef_format:format_number(1000999, 2, 2, #{thousands_sep => <<" ">>})),
 	?_assertEqual(<<"2,000,000.00">>, uef_format:format_number(2000000, 2, 2, #{thousands_sep => <<",">>})),
 	?_assertEqual(<<"9 999 999 999.00">>, uef_format:format_number(9999999999, 2, 2, #{thousands_sep => <<" ">>})),
@@ -37,9 +40,18 @@ format_number_test_() ->
 	?_assertEqual(<<"1000.88 руб."/utf8>>, uef_format:format_price(1000.8767, 4, #{cur_symbol => "руб.", cur_sep => " ", cur_pos => right})),
 	?_assertEqual(<<"€€1000.00"/utf8>>, uef_format:format_price(1000, 4, #{cur_symbol => "€", cur_sep => "€", cur_pos => left})),
 	?_assertEqual(uef_format:format_number(100, 2, 3), uef_format:format_number(100, 2, 3, #{})),
+	?_assertEqual(<<"199.500">>, uef_format:format_number(199.4567, 1, 3)),
+	?_assertEqual(<<"-199.500">>, uef_format:format_number(-199.4567, 1, 3)),
+	?_assertEqual(<<$1, 1, $0,$0,$0, $., $0,$0>>, uef_format:format_number(1000, 0, 2, #{thousands_sep => [1]})),
+	?_assertEqual(<<"1YB">>, uef_format:format_bytes(1 bsl 80, #{base => 2})),
 	?_assertEqual(uef_format:format_price(1000), uef_format:format_price(1000, 2)),
 	?_assertEqual(uef_format:format_price(1000), uef_format:format_price(1000, 2, <<>>)),
-	?_assertEqual(uef_format:format_price(1000), uef_format:format_number(1000, 2, 2, #{}))
+	?_assertEqual(uef_format:format_price(1000), uef_format:format_number(1000, 2, 2, #{})),
+	?_assertError(badarg, uef_format:format_number(1000, 0, 2, #{thousands_sep => [-1]})),
+	?_assertError(badarg, uef_format:format_number(1000, 0, 2, #{thousands_sep => 32})),
+	?_assertError({invalid_units, 'BAD_UNITS'}, uef_format:format_bytes(100, #{units => 'BAD_UNITS'})),
+	?_assertError({invalid_output_type, list}, uef_format:format_bytes(100, #{to_type => list})),
+	?_assertError({invalid_separator, " "}, uef_format:format_bytes(100, #{sep => " "}))
 	].
 
 
@@ -49,10 +61,13 @@ format_bytes_test_() ->
 	MB10_1000 = 10 * 1000 * 1000,
 	MB10_1024 = 10 * 1024 * 1024,
 	[
+	?_assertEqual(<<"0KB">>, uef_format:format_bytes(0)),
+	?_assertEqual(<<"0KB">>, uef_format:format_bytes(-0)),
 	?_assertEqual(<<"0KB">>, uef_format:format_bytes(1023)),
 	?_assertEqual(<<"1KB">>, uef_format:format_bytes(1024)),
 	?_assertEqual(<<"0KB">>, uef_format:format_bytes(999, #{base => 10})),
 	?_assertEqual(<<"1KB">>, uef_format:format_bytes(1023, #{base => 10})),
+	?_assertEqual(<<"-1KB">>, uef_format:format_bytes(-1023, #{base => 10})),
 	?_assertEqual(<<"9KB">>, uef_format:format_bytes(KB10_1000)),
 	?_assertEqual(<<"10KB">>, uef_format:format_bytes(KB10_1024)),
 	?_assertEqual(<<"10 KB">>, uef_format:format_bytes(KB10_1024, #{sep => <<" ">>})),
@@ -72,5 +87,12 @@ format_bytes_test_() ->
 	?_assertEqual({10, 'MB'}, uef_format:format_bytes(MB10_1024, #{to_type => int})),
 	?_assertEqual(<<"0MB">>, uef_format:format_bytes(1000, #{units => 'MB'})),
 	?_assertError({badarg, bad_int}, uef_format:format_bytes(bad_int)),
-	?_assertError({badarg, bad_opts}, uef_format:format_bytes(1, bad_opts))
+	?_assertError({badarg, bad_opts}, uef_format:format_bytes(1, bad_opts)),
+	?_assertError({invalid_base, 16}, uef_format:format_bytes(10000, #{base => 16}))
+	].
+
+
+format_price_test_() ->
+	[
+	?_assertError({badarg, bad_opts}, uef_format:format_price(1, 2, bad_opts))
 	].
